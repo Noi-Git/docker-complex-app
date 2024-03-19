a778276dab9ae07fc3bda1d2c61947f8367e7e31
Noi-Git <noi.patsin@gmail.com>
2024-03-18 21:21:41 -0400

    setup routes for Redis, Postgres, and React

--- server/index.js
+++ server/index.js
@@ -33,3 +33,46 @@ const redisClient = redis.createClient({
   retry_strategy: () => 1000,
 })
 const redisPublisher = redisClient.duplicate()
+
+// Express route handlers
+app.get('/', (req, res) => {
+  res.send('Hi')
+})
+// retrive all the value have ever submitted to postgres
+app.get('/values/all', async (req, res) => {
+  const values = await pgClient.query('SELECT * FROM values')
+
+  res.send(values.rows)
+})
+
+// retrive all values and all indices that have ever been submitted to the backend
+app.get('/values/current', async (req, res) => {
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values)
+  })
+})
+
+// receive new value from React application
+app.post('/values', async (req, res) => {
+  const index = req.body.index
+
+  if (parseInt(index) > 40) {
+    return res.status(422).send('The index is too high')
+  }
+
+  // put the value received into Redis data store
+  redisClient.hset('values', index, 'Nothing yet!')
+
+  //sending the input(index - we call message) to the worker process
+  //basically - pull the new value ot of Redis and start calculating
+  redisPublisher.publish('insert', index)
+
+  //add new index that just submitted into database
+  pgClient.query('INSERT INTO values(number) VALUES($1', [index])
+
+  res.send({ working: true })
+})
+
+app.listen(5000, (err) => {
+  console.log('Listinging')
+})
