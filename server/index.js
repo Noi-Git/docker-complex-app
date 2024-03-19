1c7b9727bdee68113c06ae59bfad3e8e0d60ea7d
Noi-Git <noi.patsin@gmail.com>
2024-03-18 20:49:54 -0400

    setup postgres connections

--- server/index.js
+++ server/index.js
@@ -8,3 +8,14 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
+
+// Connect to the running Postgress server
+const { Pool } = require('pg')
+const pgClient = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort,
+})
+pgClient.on('error', () => console.log('Lost PG connection'))
